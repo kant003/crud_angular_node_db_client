a6267c8a2da90a7294e194a8910b19138c01d336
kant003 <gonzalezm.angel@gmail.com>
2026-01-27 13:23:21 +0100

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { StudentCrudComponent } from './pages/student-crud/student-crud.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+      { path: '', component: StudentCrudComponent  }
+];
